938d39a391536689c1c851140ef0179a138801da
Phil Underwood <beardydoc@gmail.com>
2024-06-22 15:33:53 +0100

    Make it work!

--- action.php
+++ action.php
@@ -3,6 +3,7 @@
 use dokuwiki\Extension\ActionPlugin;
 use dokuwiki\Extension\EventHandler;
 use dokuwiki\Extension\Event;
+use dokuwiki\Subscriptions\SubscriberManager;
 
 /**
  * DokuWiki Plugin contentnotifier (Action Component)
@@ -15,7 +16,7 @@ class action_plugin_contentnotifier extends ActionPlugin
     /** @inheritDoc */
     public function register(EventHandler $controller)
     {
-        $controller->register_hook('IO_WIKIPAGE_WRITE', 'AFTER|BEFORE', $this, 'handleIoWikipageWrite');
+        $controller->register_hook('IO_WIKIPAGE_WRITE', 'BEFORE', $this, 'handleIoWikipageWrite');
     }
 
     /**
@@ -26,7 +27,51 @@ class action_plugin_contentnotifier extends ActionPlugin
      * @param mixed $param optional parameter passed when event was registered
      * @return void
      */
+     
+    private function str_contains($string, $subString) 
+    { 
+        $len = strlen($subString); 
+        if ($len == 0) { 
+            return false; 
+        } 
+        return (strpos($string, $subString) !== False); 
+    }  
+
+    private function endsWith($string, $endString) 
+    { 
+        $len = strlen($endString); 
+        if ($len == 0) { 
+            return true; 
+        } 
+        return (substr($string, -$len) === $endString); 
+    }  
+      
+     
     public function handleIoWikipageWrite(Event $event, $param)
     {
+        global $ID;
+        $filename = $event->data[0][0];
+        
+        if ($this->endsWith($filename,"gz")) {
+            // skip this file - this is saving the backup data, so ignore as duplicate
+            return;
+        }
+
+        $new_page = $event->data[0][1];
+        $content = $this->getConf('content');
+        if (! $this->getConf('case_sensitive')) {
+            $new_page = strtolower($new_page);
+            $content = strtolower($content);
+        }
+        $new_found = $this->str_contains($new_page, $content);
+        if ($new_found) {
+            \dokuwiki\Logger::error("new stuff found");
+            $users = $this->getConf('users');
+            $sub = new SubscriberManager();
+            foreach ($users as $user) {
+                \dokuwiki\Logger::error("user", $user);
+                $sub->add($ID, $user, 'every');
+            }
+        }           
     }
 }
